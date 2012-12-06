cb2465c4a0af022899935f705c20b2ffbb737e10
picasso250 <cumt.xiaochi@gmail.com>
2012-12-07 00:24:57 +0800

    add default category data insertion
    but not work

--- server/controller/index.php
+++ server/controller/index.php
@@ -1,2 +1,4 @@
 <?php
 !defined('IN_APP') && exit('ILLEGAL EXECUTION');
+
+d('hello, it works');
--- server/install/index.php
+++ server/install/index.php
@@ -4,6 +4,7 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 define('IN_APP', 1);
+exit('?');
 
 $root = __DIR__ . '/..';
 
@@ -33,6 +34,9 @@ foreach ($sqls as $sql) {
     exec_sql($sql);
 }
 
+include 'default_data.php';
+insert_categories($default_categories);
+
 function dd($str)
 {
     echo "<p>$str</p>\n";
@@ -40,11 +44,29 @@ function dd($str)
 
 function exec_sql($sql = '')
 {
+    if (empty(trim($sql)))
+        return;
     if (ON_SAE && preg_match('/USE|CREATE\sDATABASE/', $sql)) {
         return;
     }
     Pdb::exec($sql);
     $GLOBALS['histories'][] = $sql;
+}
+
+function insert_categories($cate)
+{
+    $cate = trim($cate);
+    $lines = explode(PHP_EOL, $cate);
+    foreach ($lines as $line) {
+        if (preg_match('/^\d.\s(.+)$/', $line, $matches)) {
+            Pdb::insert(array('name' => $matches[0]), 'big_category');
+            $cur_big = Pdb::lastInsertId();
+        } else if (preg_match('/^\s\d.\s(.+)$', $line, $matches)) {
+            Pdb::insert(array('big_category' => $cur_big, 'name' => $matches[0]), 'big_category');
+        } else {
+            throw new Exception("not good line formate: $line");
+        }
+    }
 }
 ?>
 <p>install ok</p>
