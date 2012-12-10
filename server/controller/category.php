747fb3d173c300b1c08359858b6cc3ca45678918
Ryan <cumt.xiaochi@gmail.com>
2012-12-11 00:07:11 +0800

    ~

--- server/controller/category.php
+++ server/controller/category.php
@@ -3,4 +3,8 @@
 
 $bigCategories = BigCategory::readArray();
 
+foreach ($bigCategories as $id => $name) {
+    $associateCategories[$id] = Category::readArray(new BigCategory($id));
+}
+
 render_view('master', array('view' => 'backend'));
