06e148a32b3f65c656d90587f964660f87ab2042
Ryan <cumt.xiaochi@gmail.com>
2012-12-10 14:03:21 +0800

    add title
    move html to static folder in test

--- server/test/index.php
+++ server/test/index.php
@@ -6,6 +6,7 @@ error_reporting(E_ALL);
 define('IN_APP', 1);
 define('ON_TEST', 1);
 
+// DS for both win and linux is not used!!!
 define('DS', DIRECTORY_SEPARATOR);
 
 define('APP_ROOT', __DIR__ . '/../');
@@ -20,28 +21,5 @@ require_once CORE_ROOT . 'function.php';
 require_once CORE_ROOT . 'app.php';
 
 init_env();
-?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html lang="zh-CN">
-<head>
-    <meta charset="UTF-8" />
-    <title>Test for Cheng</title>
-    <link rel="stylesheet" type="text/css" href="static/style.css" />
-</head>
-<body>
-    <h1><a href="?">Test for Cheng</a></h1>
-    <a href="/">exit</a>
-    <a class="clear btn" href="?exit=1">clear all side effects in db</a>
-    <div class="conclusion pass" id="pre-pass-box">ALL PASS</div>
-    <div class="conclusion fail" id="pre-fail-box">SOME FAIL!</div>
-    <ul><?php include 'test.php'; ?></ul>
-    <div class="conclusion <?= $all_pass? 'pass' : 'fail' ?>"><?= $all_pass? 'ALL PASS' : 'SOME FAIL!' ?></div>
-    <?php if ($all_pass): ?>
-        <script src="static/hide_fail.js"></script>
-    <?php endif ?>
-    
-</body>
-</html>
 
-
-
+include TEST_ROOT . 'static/index.html';
